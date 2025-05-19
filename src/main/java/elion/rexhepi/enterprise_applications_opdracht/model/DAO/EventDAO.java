f43f07ecd7a98dcfd59feb9e512ca3b009077ffa
Elion05 <elion.rexborn@gmail.com>
2025-05-19 20:15:27 +0200

    EventDAO fix, onnodige stukjes code weggehaalt

--- src/main/java/elion/rexhepi/enterprise_applications_opdracht/model/DAO/EventDAO.java
+++ src/main/java/elion/rexhepi/enterprise_applications_opdracht/model/DAO/EventDAO.java
@@ -2,14 +2,10 @@ package elion.rexhepi.enterprise_applications_opdracht.model.DAO;
 import elion.rexhepi.enterprise_applications_opdracht.model.Event;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
-import java.util.Optional;
+
 
 public interface EventDAO extends CrudRepository<Event, Integer> {
 
     //om de events op de index pagina te kunnen weergeven
     List<Event> findTop10ByOrderByTijdstipDesc();
-
-    //query parameter id
-    Optional<Event> findById(Integer id);
-
 }
